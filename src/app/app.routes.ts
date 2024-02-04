9d1525ccd261f6d7ca13a991d23b2a9dc79be1f4
Carlos Iriarte <proyamerika@gmail.com>
2024-02-04 09:45:47 -0500

    Estado incial del proyecto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
-
-NgModule({
-    imports: [
-      RouterModule.forRoot(routes, { useHash: false }),
-    ],
-    exports: [RouterModule]
-})
